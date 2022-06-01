29a232e99ef3b7928d2b7b32e927d4e2a9ab89a0
FullMarksChaheye <k191069@nu.edu.pk>
2022-06-01 08:19:01 -0700

    Some thing good happened

--- FxPractice/src/application/Main.java
+++ FxPractice/src/application/Main.java
@@ -1,16 +1,34 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			StackPane root = new StackPane();
+			
+			Button x = new Button();
+			x.setText("Abeer");
+			
+			x.setOnAction(new EventHandler<ActionEvent> () {
+
+				@Override
+				public void handle(ActionEvent arg0) {
+					System.out.println("Abeer is Good");
+				}
+				
+			});
+			
+			root.getChildren().add(x);
+			
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
